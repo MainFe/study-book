9dcc284c0393104cfe9f2e0699819bb92bfab8b3
MainFe <53177959+MainFe@users.noreply.github.com>
2024-04-12 00:35:39 +0900

    Update files

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,54 +1,10 @@
 const { fibonacci, isPowerOfTwo, isPowerOfBitWise, linearSearch, binarySearch, bubbleSort, insertionSort, recursiveBinarySearch, quickSort } = require('./src/module')
+const test = require('./src/test')
 
-// const getSomeThing = () => {
-//   return new Promise((resolve, reject) => {
-//     // fetch something
-//     resolve('some data');
-//     reject('some error');
-//   });
-// }
+test.dbConnect();
 
-// getSomeThing().then((data) => {
-//   console.log(data);
-// }, (err) => {
-//   console.log(err);
-// })
-
-// 이를 작성하는 다른 방법!
-// const re = getSomeThing().then(data => {
-//   console.log(data);
-//   return 'fucking return!';
-// }).then(date => {
-//   console.log('hey~~', date);
-// }).catch(err => {
-//   console.error('Error 발생!: ', err);
-// })
-
-// console.log(re);
-// 오류 작성시 어떻게 대처??
-
-const sqlCreate = async () => {
-  const re = await sqlDelete();
-
-  if(re.status !== 200) {
-    throw new Error("hdfashfsd");
-  }
-}
-
-const sqlDelete = () => {
-  return new Promise((resolve, reject) => {
-    reject('sqlDelete Fail');
-    resolve('sqlDelete Access');
-  })
-}
-
-const sqlite3 = async () => {
-  const as = await sqlCreate()
-  console.log(as);
-}
-
-sqlDelete()
-  .then(result => console.log(result))
-  .catch(err => console.log(err))
-
-// 리턴으로 주고싶다면, async으로, 체이닝을 하고 싶다면, Promise로 해라!! 라는 느까ㅣㅁ쓰??
+test.createMemo();
+test.searchMemo(1, {});
+test.updateMemo(1, {})
+test.deleteMemo(1);
+test.dbClose();
---
+++ JavaScript/src/test.js
@@ -0,0 +1,41 @@
+const sqlite3 = require('sqlite3').verbose();
+
+const dbPath = './sql.db'
+
+let db;
+function dbConnect() {
+  db = new sqlite3.Database(dbPath)
+}
+
+function dbClose() {
+  db.close();
+}
+
+function createMemo() {
+  console.log('create: ', 1);
+}
+
+function searchMemo(id, { tag = undefined, text = undefined, date = undefined, isOpen = undefined }) {
+  console.log('search: ', id)
+  if(tag) console.log('SELECT * FROM memo WHERE tag = ? ORDER BY date desc')
+  if(text) console.log('SELECT * FROM memo WHERE text = ? ORDER BY date desc')
+  if(date) console.log('SELECT * FROM memo ORDER BY date desc')
+  if(isOpen) console.log('SELECT * FROM memo WHERE isOpen = 1 ORDER BY date desc')
+}
+
+function updateMemo(id, { tag = undefined, text = undefined, date = undefined, isOpen = undefined }) {
+  console.log('update: ', id)
+  if(tag) console.log('UPDATE tag')
+  if(text) console.log('UPDATE text')
+  if(date) console.log('UPDATE date')
+  if(isOpen) console.log('UPDATE isOpen')
+}
+
+function deleteMemo(id) {
+  console.log('delete: ', id)
+}
+
+module.exports = { 
+  dbConnect, dbClose, 
+  createMemo,  searchMemo, updateMemo, deleteMemo,
+}
